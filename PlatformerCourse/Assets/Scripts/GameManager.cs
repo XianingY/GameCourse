7649e7e15535ec0ae7b7910ec6ed38e336dd67b9
FanYuan <1569297330@qq.com>
2024-08-01 00:17:42 +0800

    add buffer and cotoye jump,and alittle map

---
+++ PlatformerCourse/Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public Player player;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(gameObject);
+    }
+
+
+}
